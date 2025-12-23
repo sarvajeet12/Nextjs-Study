61a8e5bf5784c344c70439ce1b6b1501b9a68e04
Sarvajeet Lal Shah <132431162+sarvajeet12@users.noreply.github.com>
2025-12-23 23:10:30 +0530

    Update page.js

--- routing/src/app/blog/page.js
+++ routing/src/app/blog/page.js
@@ -8,7 +8,7 @@ export default function Blog({}){
                 All Blogs
             </h1>
             <ul>
-                <li>Blog 1</li>
+                <li>Blog </li>
                 <li>Blog 2</li>
                 <li>Blog 3</li>
                 <li>Blog 4</li>
